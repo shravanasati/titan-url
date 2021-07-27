006da0612bf58e9335cf47aa008c92cd68aa0810
Shravan <69118069+Shravan-1908@users.noreply.github.com>
2021-07-27 18:55:02 +0530

    custom aliases + support for titan-url-cli (#5)
    * 🚧 did some changes for a single page app
    
    * 💄 aligned copy button
    
    * 🔇 removed console logs
    
    * ✏️ fixed typo
    
    * ✨ custom aliases backend
    
    * ✅ frontend implementation of new API
    
    * 🐛 fixed url validation bug
    
    * ➕ add requests as dependency
    
    * 📝 add readme
    
    * ⚡️ complete the migration to a single page app and backend APIs
    
    * 🔒️ change post data type to json from form

--- app.py
+++ app.py
@@ -1,59 +1,135 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, jsonify
 import sqlite3
 from random import choice
 from string import ascii_letters, digits
-import re
+from requests.models import PreparedRequest
+from requests.exceptions import MissingSchema
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+
+def check_url(url: str) -> bool:
+    prepared_request = PreparedRequest()
+    try:
+        prepared_request.prepare_url(url, None)
+        return True
+    except MissingSchema as e:
+        return False
+
+
 @app.route("/")
 def home():
-	return render_template('index.html', URL="The shortened URL will appear here", scroll="no")
+    return render_template('index.html', URL="The shortened URL will appear here", scroll="no")
+
+def is_slug_used(slug:str) -> bool:
+    """
+    Returns a boolean value whether the given slug has been used or not.
+    """
+    conn = sqlite3.connect("./urls.db")
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS urls(original_url text, slug text)")
+    c.execute("SELECT (slug) FROM urls")
+    used_slugs = {i[0] for i in c.fetchall()}
+
+    return slug in used_slugs
 
 
-@app.route("/shorten", methods=['GET'])
+@app.route("/shorten", methods=['GET', 'POST'])
 def shorten():
-	try:
-		url = request.args.get("original-url")
-		print("URL", url)
-		# alias_type = request.form['alias-type']
+    try:
+        if request.method == "GET":
+            return render_template("index.html")
 
-		if len(re.findall(r"[http|https|ftp]://\w", url)) == 0:
-			return render_template('404.html')
+        data = request.json
+        if data is None:
+            return jsonify(
+                {
+                    "ok": False,
+                    "message": "Please provide a valid JSON object."
+                }
+            )
 
-		slug = ""
-		for _ in range(6):
-			slug += choice([choice(digits), choice(ascii_letters)])
-		print("SLUG",slug)
-		conn = sqlite3.connect("./urls.db")
-		c = conn.cursor()
-		c.execute("CREATE TABLE IF NOT EXISTS urls(original_url text, slug text)")
-		c.execute("INSERT INTO urls VALUES(:url, :slug)", {"url":url, "slug":slug})
-		conn.commit()
-		conn.close()
-		return render_template('index.html', URL=f"{request.host_url}/{slug}", scroll="yes")
+        url = data.get("original-url")
+        alias_type = data.get("alias-type")
+        print("URL", url)
+        print("Alias Type", alias_type)
+        if url is None or alias_type is None:
+            return jsonify({
+                "ok": False,
+                "message": "Missing fields `original-url` or `alias-type`."
+            })
 
+        if not check_url(url):
+            return jsonify({
+                "ok": False,
+                "message": "The entered URL is invalid."
+            })
 
-	except Exception as e:
-		print(e)
-		return render_template('404.html')
+        if alias_type == "random":
+            slug = "".join([choice([choice(digits), choice(ascii_letters)]) for _ in range(6)])
+    
+            slug_used = is_slug_used(slug)
+            while slug_used:
+                slug = "".join([choice([choice(digits), choice(ascii_letters)]) for _ in range(6)])
+                slug_used = is_slug_used(slug)
+
+        elif alias_type == "custom":
+            slug = data.get("slug")
+            if slug is None:
+                return jsonify({
+                    "ok": False,
+                    "message": "Missing field `slug`."
+                })
+
+            if is_slug_used(slug):
+                return jsonify({
+                    "ok": False,
+                    "message": "This custom alias has already been used! Try another one."
+                })
+
+        else:
+            return jsonify({
+                "ok": False,
+                "message": "Invalid alias type!"
+            })
+
+        print("SLUG", slug)
+
+        conn = sqlite3.connect("./urls.db")
+        c = conn.cursor()
+        c.execute("CREATE TABLE IF NOT EXISTS urls(original_url text, slug text)")
+        c.execute("INSERT INTO urls VALUES(:url, :slug)",
+                  {"url": url, "slug": slug})
+        conn.commit()
+        conn.close()
+
+        return jsonify({
+            "ok": True,
+            "message": f"{request.host_url}{slug}"
+        })
+
+    except Exception as e:
+        print(e)
+        return render_template('404.html')
+
 
 @app.route("/<string:slug>")
 def get(slug):
-	conn = sqlite3.connect("./urls.db")
-	c = conn.cursor()
-	c.execute("CREATE TABLE IF NOT EXISTS urls(original_url text, slug text)")
-	c.execute("SELECT * FROM urls WHERE slug = :slug", {"slug":slug})
-	try:
-		url = c.fetchone()[0]
-		print(url)
-		return redirect(url)
-	except Exception as e:
-		print(e)
-		return render_template("404.html")
-	finally:
-		conn.close()
+    conn = sqlite3.connect("./urls.db")
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS urls(original_url text, slug text)")
+    c.execute("SELECT * FROM urls WHERE slug = :slug", {"slug": slug})
+    try:
+        url = c.fetchone()[0]
+        print(url)
+        return redirect(url)
+    except Exception as e:
+        print(e)
+        return render_template("404.html")
+    finally:
+        conn.close()
+
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,4 +1,3 @@
-console.log("grubgrbui");
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -14,3 +13,55 @@ async function copyToClipboard() {
 
 let copyBtn = document.getElementById("copyBtn")
 copyBtn.addEventListener("click", copyToClipboard)
+
+const getRequestData = () => {
+    const url = document.getElementById("original-url").value
+    let aliasType = document.getElementById("alias").value
+    let slug = document.getElementById("slug").value
+
+    console.log(url);
+    console.log(aliasType);
+    console.log(slug);
+
+    return {
+        "original-url": url,
+        "alias-type": aliasType,
+        "slug": slug
+    }
+}
+
+async function shorten() {
+    console.log("shortening...");
+    let requestData = getRequestData()
+
+    console.log(typeof JSON.stringify(requestData));
+    console.log(JSON.stringify(requestData));
+
+    await fetch("/shorten", {
+        method: 'POST',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData),
+    })
+
+    .then(response => {return response.json()})
+
+    .then(data => {
+        let urlText = document.getElementById("result-url")
+        urlText.innerText = data["message"]
+        urlText.scrollIntoView()
+    })
+
+    .catch(err => {
+        let urlText = document.getElementById("result-url")
+        urlText.innerText = "An error occured! Check your internet connection."
+        urlText.scrollIntoView()
+    })
+
+}
+
+
+let shortenBtn = document.getElementById("shortenBtn")
+shortenBtn.addEventListener("click", shorten)
