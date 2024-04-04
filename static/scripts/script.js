2f10599d5788a51a52e56063579a36aa4f1c3fc3
Shravan Asati <dev.shravan@protonmail.com>
2024-04-04 12:06:52 +0530

    result stats

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -19,10 +19,6 @@ const getRequestData = () => {
     let alias = document.getElementById("alias").value
     let aliasType = (alias) ? "custom" : "random";
 
-    console.log(url);
-    console.log(aliasType);
-    console.log(alias);
-
     return {
         "original-url": url,
         "alias-type": aliasType,
@@ -35,14 +31,21 @@ async function shorten(ev) {
     console.log("shortening...");
     let requestData = getRequestData()
     let urlText = document.querySelector("#result-url");
+    let shortenBtn = document.querySelector("#shortenBtn")
+    let resultStat = document.querySelector("#result-stat");
+
+    shortenBtn.disabled = true;
+    shortenBtn.value = "Please wait...";
+    resultStat.innerText = "";
+
     if (!requestData["original-url"]) {
         urlText.innerText = "Please enter a URL.";
-        urlText.scrollIntoView();
+        shortenBtn.scrollIntoView();
         return;
     }
     if (requestData["alias-type"] === "custom" && !requestData["alias"].match(/^(?=.*[A-Za-z0-9])[\w\-]{1,50}$/gm)) {
         urlText.innerText = "Invalid alias! It must only contain alphanumeric characters, hyphens (-), underscores (_), and not be longer than 50 characters.";
-        urlText.scrollIntoView();
+        shortenBtn.scrollIntoView();
         return;
     }
 
@@ -58,22 +61,27 @@ async function shorten(ev) {
         .then(response => { return response.json() })
 
         .then(data => {
+            console.log(data);
             urlText.innerText = data["message"]
-            urlText.scrollIntoView()
-            
-            let lenOriginal = requestData["original-url"].length;
-            let lenShortened = data["message"].length;
-            let diff = lenOriginal - lenShortened;
-            if (diff >= 0) {
-                console.log(`link shortened by ${diff / lenOriginal * 100}%`);
-            } else {
-                console.log(`link made longer by ${diff / lenOriginal * 100}%`);
+            shortenBtn.scrollIntoView()
+            shortenBtn.disabled = false;
+            shortenBtn.value = "Shorten";
+
+            if (data["ok"]) {
+                let lenOriginal = requestData["original-url"].length;
+                let lenShortened = data["message"].length;
+                let diff = lenOriginal - lenShortened;
+                if (diff >= 0) {
+                    resultStat.innerText = `Link shortened by ${Math.round(diff / lenOriginal * 100)}% 🤠`;
+                } else {
+                    resultStat.innerText = `Link made longer by ${Math.round(Math.abs(diff) / lenOriginal * 100)}% 😞`;
+                }
             }
         })
 
         .catch(err => {
             urlText.innerText = "An error occured! Check your internet connection."
-            urlText.scrollIntoView()
+            shortenBtn.scrollIntoView()
         })
 
 }
